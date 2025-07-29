f85387e938010200ac208be6b66f6e6b4db631d8
Emil Bogaci <140043844+emilos2001@users.noreply.github.com>
2025-07-29 13:26:35 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,28 +1,11 @@
 import {createRoot} from "react-dom/client";
-// import {MusicPads} from './MusicPads/Pads.jsx'
-// import {Jokes} from "./index.jsx";
-// import {HeaderApp} from "./TravelJournal/header.jsx";
 import {ChefClaude} from "./ChefClaude/Header.jsx";
-// import {Counter} from "./Counter/Counter.jsx";
-// import {App} from './MemGenerator/App.jsx'
-// import {AIApp} from "./OpenAIAPI/AIApp.jsx";
+
 
 
 const root = createRoot(document.getElementById('root'))
 root.render(
     <>
-        {/*<AIApp/>*/}
-        {/*<App/>*/}
-        {/*<Counter/>*/}
-        {/*<Jokes/>*/}
-        {/*<HeaderApp/>*/}
-        {/*<Validation/>*/}
-        {/*<MusicPads />*/}
-        {/*<Ijoistro/>*/}
         <ChefClaude/>
-        {/*<HeaderApp/>*/}
-        {/*<App/>*/}
-        {/*<Challenge/>*/}
-        {/*{<NinjaTurtle/>}*/}
     </>
 )
